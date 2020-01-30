21163b924939cc6987bf6d47100549cff6684f90
Awluple <cdqwerty1@gmail.com>
2020-01-30 17:20:51 +0100

    improved admin site

--- src/RotcetDjango/screenings/admin.py
+++ src/RotcetDjango/screenings/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import Screening, Show, Room
 
-admin.site.register(Screening)
-admin.site.register(Show)
+@admin.register(Screening)
+class ScreeningAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'show', 'room')
+    autocomplete_fields = ('show',)
+    ordering = ['date']
+    search_fields = ['show__movie__name','show__marathon__title', 'date']
+
+@admin.register(Show)
+class ShowAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'type')
+    list_filter = ('type',)
+    autocomplete_fields = ('movie', 'marathon')
+    search_fields = ('movie__name', 'marathon__title')
+    
+
 admin.site.register(Room)
