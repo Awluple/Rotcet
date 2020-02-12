4ea5e71d7179ad79ed4e334d7117189365b4bb45
Awluple <cdqwerty1@gmail.com>
2020-02-12 17:33:58 +0100

    home component added

--- src/RotcetReact/src/App.js
+++ src/RotcetReact/src/App.js
@@ -4,12 +4,14 @@ import './scss/App.scss';
 import {BrowserRouter} from 'react-router-dom'
 
 import Top from './components/top/top.jsx'
+import Main from './components/main/main.jsx'
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
         <Top />
+        <Main />
     </div>
     </BrowserRouter>
   );
