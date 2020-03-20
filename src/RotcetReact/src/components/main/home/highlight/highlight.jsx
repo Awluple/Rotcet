1d16b1d55dd6768efaa1c20469cde966c44d2047
Awluple <cdqwerty1@gmail.com>
2020-03-20 16:59:04 +0100

    receives and displays highlighted movies

--- src/RotcetReact/src/components/main/home/highlight/highlight.jsx
+++ src/RotcetReact/src/components/main/home/highlight/highlight.jsx
@@ -1,21 +1,49 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 
+import LoadingGif from 'media/gifs/loading.jsx'
+
+import Movie from './movie.jsx'
+
 const Highlight = () => {
 
     const [highlights, setHighlights] = useState(null)
 
-    useEffect(() => {
-        axios.get('/api/movies', {
+    const getMovies = () => {
+        return axios.get('/api/movies', {
             params: {
-                
+                'highlight': true,
+                'fields': 'id,name,main_trailer,main_image,short_description'
             }
+        }).then(
+            res => { return res.data.results }
+        )
+    }
+
+    useEffect(() => {
+        getMovies().then(data => {
+            setHighlights(data)
         })
     }, [])
 
+    if(highlights === null){
+        return (
+            <div className='highlight highlight--no_movies'>
+                <LoadingGif />
+            </div>
+        )
+    }else if (highlights.length === 0){
+        return null
+    }
+
     return (
-        <div>
-            
+        <div className='highlight'>
+            <h2 className='header header--big'>Highlight</h2>
+            <div className='highlight__movies'>
+                {highlights.map((movie, index) => {
+                    return <Movie key={movie.id} movie={movie} index={index} />
+                })}
+            </div>
         </div>
     )
 }
---
+++ src/RotcetReact/src/components/main/home/highlight/movie.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Movie = props => {
+    console.log(props.index)
+    return (
+        <div className={'highlight_movie' + ((props.index === 1) ? ' highlight_movie--left' : '')}>
+            <div className='movie__graphic'>
+                {props.movie.main_trailer ? 
+                    <iframe src={props.movie.main_trailer} frameBorder="0"></iframe>
+                :
+                    <img src={props.movie.main_image} alt="movie main image"/>
+                }
+            </div>
+            <div className='movie__description'>
+                <p>{props.movie.short_description}</p>
+            </div>
+        </div>
+    )
+}
+
+Movie.propTypes = {
+    movie: PropTypes.object.isRequired,
+    index: PropTypes.number.isRequired
+}
+
+export default Movie
