5302b7d614c5ea32e5046a1d31a4c6b8882d4ad3
Awluple <cdqwerty1@gmail.com>
2020-03-21 20:51:45 +0100

    added border-radius animation, changed database trailers table from iframe link to YouTube video ID

--- src/RotcetReact/src/components/main/home/highlight/movie.jsx
+++ src/RotcetReact/src/components/main/home/highlight/movie.jsx
@@ -1,16 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 import {Link} from 'react-router-dom'
+import YouTube from 'react-youtube';
 
 const Movie = props => {
-    console.log(props.index)
+    
+    const [iframeClicked, setiframeClicked] = useState(false)
+
+
+    let style
+
+    if(iframeClicked){
+        style = {
+            borderRadius: '0px'
+        }
+    }
+
+    const opts = {
+        width: '675px',
+        height: '380px'
+    }
+
+    const setBorder = () => {
+        setiframeClicked(!iframeClicked)
+    }
+
     return (
         <div className={'highlight_movie' + ((props.movie.main_trailer === null) ? ' highlight_movie--no_trailer' : '') + ((props.index === 1) ? ' highlight_movie--left' : '') }>
-            <div className='movie__graphic'>
+            <div className={'movie__graphic' + ((iframeClicked && props.movie.main_trailer) ? ' movie__graphic--remove-border-radius' : '')}>
                 {props.movie.main_trailer ? 
-                    <iframe src={props.movie.main_trailer} frameBorder="0"></iframe>
+                    <YouTube videoId={props.movie.main_trailer} opts={opts} 
+                    onPlay={setBorder}
+                    onPause={setBorder}
+                    onEnd={setBorder}
+                    />
                 :
-                    <img src={props.movie.thumbnail} alt="movie main image"/>
+                   <img src={props.movie.thumbnail} alt="movie main image"/>
                 }
             </div>
             <div className='movie__description'>
