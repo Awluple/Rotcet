ce4c3ced0f1f2bd03a9a8698ae968f2e2b9e846e
Awluple <cdqwerty1@gmail.com>
2020-04-10 13:53:37 +0200

    movie tickets_sale_date field may now be empty

---
+++ src/RotcetReact/src/components/main/home/upcoming/upcoming.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import axios from 'axios'
+
+const Upcoming = () => {
+    return (
+        <div className='upcoming'>
+            <h2 className='header header--medium'>COMING SOON</h2>
+        </div>
+    )
+}
+
+export default Upcoming
