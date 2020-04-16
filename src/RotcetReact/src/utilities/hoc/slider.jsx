012535e3169362519d05c7e7861f49911ea00f7d
Awluple <cdqwerty1@gmail.com>
2020-04-16 14:11:02 +0200

    added nav section responsivness

--- src/RotcetReact/src/utilities/hoc/slider.jsx
+++ src/RotcetReact/src/utilities/hoc/slider.jsx
@@ -12,17 +12,19 @@ const Slider = props => {
             setPosition(100)
         }
         setTimeout(() => {
+            document.body.style.overflow = 'auto' // enable scrolling
             props.close(false)
         }, 100);
     }
 
     useEffect(() => {
         setPosition(0)
+        document.body.style.overflow = 'hidden' // disable scrolling
     }, [])
 
     return (
         <div className={'slider ' + (props.from === 'left' ? 'slider--left' : 'slider--right') } style={{left: position + '%'}}>
-            <div onClick={hide} className='slider__close'>
+            <div className='slider__close'>
                 <svg onClick={hide} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.151 17.943l-4.143-4.102-4.117 4.159-1.833-1.833 4.104-4.157-4.162-4.119 1.833-1.833 4.155 4.102 4.106-4.16 1.849 1.849-4.1 4.141 4.157 4.104-1.849 1.849z"/>
                 </svg>
