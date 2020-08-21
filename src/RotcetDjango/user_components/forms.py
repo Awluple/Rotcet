9ddc9335ef1f4b7ceb6b1e0ffefd79b38c4e89a1
Awluple <cdqwerty1@gmail.com>
2020-08-21 22:38:45 +0200

    registration and ragistration form validation

--- src/RotcetDjango/user_components/forms.py
+++ src/RotcetDjango/user_components/forms.py
@@ -3,12 +3,28 @@ from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 from django.utils.translation import gettext as _
 from django.contrib import messages
+from django.contrib.auth.password_validation import get_default_password_validators
 
+def validate_password(password, user=None, password_validators=None):
+    """
+    Django password validator which returns errors instead of calling them
+
+    """
+    errors = []
+    if password_validators is None:
+        password_validators = get_default_password_validators()
+    for validator in password_validators:
+        try:
+            validator.validate(password, user)
+        except ValidationError as error:
+            errors.append(error)
+    
+    return errors
 
 class UserRegistrationForm(forms.Form):
     email = forms.EmailField(max_length=150, widget=forms.TextInput(attrs={'placeholder': 'Email'}))
-    password1 = forms.CharField(max_length=24, min_length=5, widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
-    password2 = forms.CharField(max_length=24, min_length=5, widget=forms.PasswordInput(attrs={'placeholder': 'Confirm password'}))
+    password = forms.CharField(max_length=24, min_length=8, widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    password_confirmation = forms.CharField(max_length=24, min_length=8, widget=forms.PasswordInput(attrs={'placeholder': 'Confirm password'}))
 
     def clean_email(self):
         email = self.cleaned_data['email']
@@ -17,11 +33,24 @@ class UserRegistrationForm(forms.Form):
             raise ValidationError(_("There is already an account with this email"), code='invalid')
         return email
 
+
     def clean(self):
         cleaned_data = super().clean()
-        password1 = cleaned_data.get('password1')
-        password2 = cleaned_data.get('password2')
+        password = self.cleaned_data.get('password')
+        password_confirmation = self.cleaned_data.get('password_confirmation')
 
-        if password1 and password2:
-            if password1 != password2:
-                raise ValidationError(_("Passwords do not match"), code='invalid')
+        errors = []
+
+        if password and password_confirmation:
+            if password != password_confirmation:
+                errors.append(ValidationError(_("Passwords do not match"), code='invalid'))
+        else:
+            errors.append(ValidationError(_("Enter both the password and the password confirmation"), code='empty'))
+        
+        password_errors = validate_password(password)
+
+        if password_errors:
+            errors.append(password_errors)
+
+        if errors:
+            raise ValidationError(errors)
--- src/RotcetDjango/user_components/views.py
+++ src/RotcetDjango/user_components/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as user_login
+from django.contrib.auth.models import User
 from django.contrib import messages
 
 from .forms import UserRegistrationForm
@@ -26,6 +27,14 @@ def login(request):
 def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
+
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            User.objects.create_user(username=email, email=email, password=password)
+            user = authenticate(request, email=email, password=password)
+            user_login(request, user)
+            return redirect('/')
 
         return render(request, 'register.html', {'form': form})
 
