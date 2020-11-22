714b327fe6c07fda7286c900e22a2cc624996ce0
Awluple <cdqwerty1@gmail.com>
2020-11-22 18:18:08 +0000

    added basic funtions for radio inputs

--- src/RotcetReact/src/components/main/tickets/ticketsType.jsx
+++ src/RotcetReact/src/components/main/tickets/ticketsType.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import TypeSelection from './typeSelection.jsx'
+
 const TicketsType = props => {
     return (
         <div className='tickets__type'>
@@ -9,22 +11,7 @@ const TicketsType = props => {
                 <ul>
                     {props.chosenSeats.map((seat, index) => {
                         return (
-                            <li key={seat}>
-                                <div className='tickets__seat'>
-                                    <p>Ticket {index + 1}</p>
-                                    <p>Seat {seat}</p>
-                                </div>
-                                <div className='tickets__select'>
-                                        <input id={`ticket-${seat}-1`} name={`ticket-${seat}`} type="radio"/>
-                                        <label htmlFor={`ticket-${seat}-1`}></label>
-                                        
-                                        <input id={`ticket-${seat}-2`} name={`ticket-${seat}`} type="radio"/>
-                                        <label htmlFor={`ticket-${seat}-2`}></label>
-
-                                        <input id={`ticket-${seat}-3`} name={`ticket-${seat}`} type="radio"/>
-                                        <label htmlFor={`ticket-${seat}-3`}></label>
-                                </div>
-                            </li>
+                            <TypeSelection key={seat} seat={seat} index={index} />
                         )
                     })}
                 </ul>
---
+++ src/RotcetReact/src/components/main/tickets/typeSelection.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
+const TypeSelection = props => {
+
+    const [type, setType] = useState(1)
+
+    const validateType = (type) => {
+        setType(type)
+    }
+
+    return (
+        <div>
+            <li key={props.seat}>
+                <div className='tickets__seat'>
+                    <p>Ticket {props.index + 1}</p>
+                    <p>Seat {props.seat}</p>
+                </div>
+                <div className='tickets__select'>
+                        <input id={`ticket-${props.seat}-1`} name={`ticket-${props.seat}`}
+                        value={1} type="radio" checked={type === 1}
+                        checked={type === 1}
+                        onChange={() => {return}}/>
+                        <label htmlFor={`ticket-${props.seat}-1`} onClick={() => {validateType(1)}}></label>
+                        
+                        <input id={`ticket-${props.seat}-2`} name={`ticket-${props.seat}`}
+                        value={2} type="radio" checked={type === 2}
+                        checked={type === 2}
+                        onChange={() => {return}}/>
+                        <label htmlFor={`ticket-${props.seat}-2`} onClick={() => {validateType(2)}}></label>
+
+                        <input id={`ticket-${props.seat}-3`} name={`ticket-${props.seat}`}
+                        value={3} type="radio" checked={type === 3}
+                        checked={type === 3}
+                        onChange={() => {return}}/>
+                        <label htmlFor={`ticket-${props.seat}-3`} onClick={() => {validateType(3)}}></label>
+                </div>
+            </li>
+        </div>
+    )
+}
+
+TypeSelection.propTypes = {
+    seat: PropTypes.number.isRequired,
+    index: PropTypes.number.isRequired
+}
+
+export default TypeSelection
